0fcb2957954c5788daf7ddc4ab31ccf25e81ebaf
fadelyking <fadely195@gmail.com>
2024-01-06 06:42:28 +0300

    setup routes for the sign up page

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,14 @@
 var express = require("express");
 var router = express.Router();
 
-/* GET home page. */
+const message_controller = require("../controllers/message");
+const user_controller = require("../controllers/user");
+
 router.get("/", function (req, res, next) {
-	res.render("index", { title: "Express" });
+	res.render("index");
 });
+
+router.get("/sign-up", user_controller.user_create_get);
+router.post("/sign-up", user_controller.user_create_post);
 
 module.exports = router;
