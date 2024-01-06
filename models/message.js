8d700cb18ae0cd333c5841c5e33d50f508ee3909
fadelyking <fadely195@gmail.com>
2024-01-06 06:59:21 +0300

    few changes to message structure

--- models/message.js
+++ models/message.js
@@ -3,7 +3,6 @@ const Schema = mongoose.Schema;
 
 const MessageSchema = new Schema({
 	title: String,
-	timestamp: Timestamp,
 	text: String,
 	user: { type: Schema.Types.ObjectId, ref: "User" },
 });
