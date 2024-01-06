246d2678cda15262aa8660dc84ee4c8b2b51a6c8
fadelyking <fadely195@gmail.com>
2024-01-06 06:41:46 +0300

    added an extra field and a virtual

--- models/user.js
+++ models/user.js
@@ -2,10 +2,15 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const UserSchema = new Schema({
-	name: String,
+	first_name: String,
 	last_name: String,
 	email: String,
+	password: String,
 	membership_status: Number,
+});
+
+UserSchema.virtual("full_name").get(function () {
+	return `${this.first_name} ${this.last_name}`;
 });
 
 module.exports = mongoose.model("User", UserSchema);
