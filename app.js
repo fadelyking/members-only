9ffccb3aa66e431abd1ba5b1d685da45a547b070
fadelyking <fadely195@gmail.com>
2024-01-08 12:10:21 +0300

    securing passwords with bcrypt and passportjs

--- app.js
+++ app.js
@@ -4,7 +4,10 @@ const express = require("express");
 const path = require("path");
 const cookieParser = require("cookie-parser");
 const logger = require("morgan");
-
+const User = require("./models/user.js");
+const session = require("express-session");
+const passport = require("passport");
+const LocalStrategy = require("passport-local").Strategy;
 const indexRouter = require("./routes/index");
 const mongoose = require("mongoose");
 mongoose.set("strictQuery", "false");
@@ -15,7 +18,10 @@ async function main() {
 	await mongoose.connect(mongoDB);
 }
 const app = express();
-
+app.use(session({ secret: "cats", resave: false, saveUninitialized: true }));
+app.use(passport.initialize());
+app.use(passport.session());
+app.use(express.urlencoded({ extended: false }));
 // view engine setup
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
@@ -31,6 +37,36 @@ app.use("/", indexRouter);
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
 	next(createError(404));
+});
+
+passport.use(
+	new LocalStrategy(async (username, password, done) => {
+		try {
+			const user = await User.findOne({ username: username });
+			if (!user) {
+				return done(null, false, { message: "Incorrect username" });
+			}
+			if (user.password !== password) {
+				return done(null, false, { message: "Incorrect password" });
+			}
+			return done(null, user);
+		} catch (err) {
+			return done(err);
+		}
+	})
+);
+
+passport.serializeUser((user, done) => {
+	done(null, user.id);
+});
+
+passport.deserializeUser(async (id, done) => {
+	try {
+		const user = await User.findById(id);
+		done(null, user);
+	} catch (err) {
+		done(err);
+	}
 });
 
 // error handler
--- controllers/user.js
+++ controllers/user.js
@@ -1,5 +1,6 @@
 const Message = require("../models/message");
 const User = require("../models/user");
+const bcryptjs = require("bcryptjs");
 const asyncHandler = require("express-async-handler");
 const { body, validationResult } = require("express-validator");
 
@@ -18,30 +19,39 @@ exports.user_create_post = [
 		.escape(),
 	body("email", "Please correct email").trim().isEmail().escape(),
 	body("password", "Password Must contain at least 3 characters").trim().escape(),
+	body("confirmPassword", "Passwords do not match").custom((value, { req }) => {
+		return value === req.body.password;
+	}),
 
 	asyncHandler(async (req, res, next) => {
-		const errors = validationResult(req);
+		bcryptjs.hash(req.body.password, 10, async (err, hashedPassword) => {
+			try {
+				const errors = validationResult(req);
 
-		const user = new User({
-			first_name: req.body.name,
-			last_name: req.body.name,
-			email: req.body.email,
-			password: req.body.password,
-			membership_status: 0,
-		});
+				const user = new User({
+					first_name: req.body.name,
+					last_name: req.body.name,
+					email: req.body.email,
+					password: hashedPassword,
+					membership_status: 0,
+				});
 
-		if (!errors.isEmpty()) {
-			res.render("user_form", { errors: errors.array() });
-			return;
-		} else {
-			const userExists = await User.findOne({ first_name: req.body.first_name });
+				if (!errors.isEmpty()) {
+					res.render("user_form", { errors: errors.array() });
+					return;
+				} else {
+					const userExists = await User.findOne({ first_name: req.body.first_name });
 
-			if (userExists) {
-				res.render("user_form", { errors: "User Already Exists !" });
-			} else {
-				await user.save();
-				res.render("user_form");
+					if (userExists) {
+						res.render("user_form", { errors: "User Already Exists !" });
+					} else {
+						await user.save();
+						res.render("user_form");
+					}
+				}
+			} catch (err) {
+				return next(err);
 			}
-		}
+		});
 	}),
 ];
